04a80b37e8b7359ebc65d5648fcf6b69c9ec097e
hayden langelier <haydenlangelier@haydens-MacBook-Pro.local>
2018-10-30 17:56:06 -0700

    Working Version with Current CleverTap SDK

--- TM CT Demo/FirstViewController.swift
+++ TM CT Demo/FirstViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import CleverTapSDK
+
+
 
 class FirstViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .blue
         // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let profile: Dictionary<String, String> = [
+            //Update pre-defined profile properties
+            "Name": "Hayden Langelier",
+            "Email": "hayden.langelier@gmail.com",
+            //  Update custom profile properties
+            "Plan type": "Silver",
+            "Favorite Food": "Pizza"
+        ]
+        
+        CleverTap.sharedInstance()?.profilePush(profile)
+        createButton()
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    func createButton () {
+        let button = UIButton();
+        button.setTitle("CleverTap, Click Me!", for: .normal)
+        button.setTitleColor(UIColor.red, for: .normal)
+        button.frame = CGRect(x: 15, y: 50, width: 200, height: 100)
+        button.center = self.view.center
+        button.addTarget(self, action: #selector(buttonAction(sender:)), for: .touchUpInside)
+        
+        self.view.addSubview(button)
+    }
+    
+    
+    
+    
+    @objc func buttonAction(sender: UIButton!) {
+        print("Button tapped")
+        CleverTap.sharedInstance()?.recordEvent("Button Clicked")
+    }
 }
 
